5170bd248db46a33a0057429967d1ea80ce8dec0
Madelas <madelas797@protonmail.ch>
2025-02-16 11:19:32 -0500

    Add: Lesson 5 in class code and edits
    Feat: Magic projectile collision is updated, treasure chest BP is added
    along with animation to open/close it, lever to interact with is added

--- Source/ActionRogueLike/Public/SItemChest.h
+++ Source/ActionRogueLike/Public/SItemChest.h
@@ -29,7 +29,7 @@ protected:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* BaseMesh;
 
-	UPROPERTY(VisibleAnywhere)
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere)
 	UStaticMeshComponent* LidMesh;
 
 public:	
