7b851e0fdcd82541cbde545b4813d81f8d9d1a42
Madelas <madelas797@protonmail.ch>
2025-10-05 21:06:38 -0400

    Add: lesson 11 code/content add: code for Assigment 3; not complete. Gonna look through TL repo for the rest of the code to add

--- Source/ActionRogueLike/Private/SAttributeComponent.cpp
+++ Source/ActionRogueLike/Private/SAttributeComponent.cpp
@@ -1,26 +1,50 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "SAttributeComponent.h"
 
 // Sets default values for this component's properties
 USAttributeComponent::USAttributeComponent()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
+	// Set this component to be initialized when the game starts, and to be ticked
+	// every frame.  You can turn these features off to improve performance if you
+	// don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
+	HealthMax = 100;
+	Health = HealthMax;
 	// ...
 }
 
-bool USAttributeComponent::ApplyHealthChange(float Delta)
-{	
+bool
+USAttributeComponent::ApplyHealthChange(float Delta)
+{
 	Health += Delta;
-	OnHealthChanged.Broadcast(nullptr, this, Health, Delta);
-	return true;
+
+	float OldHealth = Health;
+
+	Health = FMath::Clamp(Health + Delta, 0.0f, HealthMax);
+
+
+	float ActualDelta = Health - OldHealth;
+	OnHealthChanged.Broadcast(nullptr, this, Health, ActualDelta); // @fixme: Still nullptr for InstigatorActor parameter
+
+	return ActualDelta;
 }
 
-bool USAttributeComponent::IsAlive() const
+bool
+USAttributeComponent::IsAlive() const
 {
 	return Health > 0.0f;
+}
+
+bool
+USAttributeComponent::IsFullHealth() const
+{
+	return Health == HealthMax;
+}
+
+bool
+USAttributeComponent::GetHealthMax() const
+{
+	return HealthMax;
 }
--- Source/ActionRogueLike/Public/SAttributeComponent.h
+++ Source/ActionRogueLike/Public/SAttributeComponent.h
@@ -2,34 +2,51 @@
 
 #pragma once
 
-#include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "CoreMinimal.h"
 #include "SAttributeComponent.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnHealthChanged, AActor*, InstigatorActor, USAttributeComponent*, OwningComp, float, NewHealth, float, Delta);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FOnHealthChanged,
+                                              AActor*,
+                                              InstigatorActor,
+                                              USAttributeComponent*,
+                                              OwningComp,
+                                              float,
+                                              NewHealth,
+                                              float,
+                                              Delta);
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class ACTIONROGUELIKE_API USAttributeComponent : public UActorComponent
 {
-	GENERATED_BODY()
+  GENERATED_BODY()
 
-public:	
-	// Sets default values for this component's properties
-	USAttributeComponent();
+public:
+  // Sets default values for this component's properties
+  USAttributeComponent();
 
 protected:
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes")
+  float Health = 100;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Attributes")
-	float Health = 100;
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Attributes")
+  float HealthMax;
 
-public:	
+public:
+  UPROPERTY(BlueprintAssignable)
+  FOnHealthChanged OnHealthChanged;
 
-	UPROPERTY(BlueprintAssignable)
-	FOnHealthChanged OnHealthChanged;
-	
-	UFUNCTION(BlueprintCallable, Category = "Attributes")
-	bool ApplyHealthChange(float Delta);
+  UFUNCTION(BlueprintCallable, Category = "Attributes")
+  bool ApplyHealthChange(float Delta);
 
-	UFUNCTION(BlueprintCallable, Category = "Attributes")
-	bool IsAlive() const;
+  UFUNCTION(BlueprintCallable, Category = "Attributes")
+  bool IsAlive() const;
+
+  UFUNCTION(BlueprintCallable)
+  bool IsFullHealth() const;
+  
+  UFUNCTION(BlueprintCallable)
+  bool GetHealthMax() const;
+
+
 };
